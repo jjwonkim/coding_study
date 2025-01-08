1dad53a7c90daabf5b0d8410d50750c56f98944e
Jaewon Kim <jjwon.kim@gmail.com>
2025-01-08 23:55:28 +0900

    14. useDebounce Custom Hooks 만들기기

--- react-pokemon-app/src/App.jsx
+++ react-pokemon-app/src/App.jsx
@@ -4,6 +4,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import PokeCard from "./components/PokeCard";
+import { useDebounce } from "./hooks/useDebounce";
 
 function App() {
   const [pokemons, setPokemon] = useState([]);
@@ -11,10 +12,16 @@ function App() {
   const [limit, setLimit] = useState(20);
   const [searchTerm, setSearchTerm] = useState("");
 
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
   useEffect(() => {
     // API => response => state update => component rerender => state
     fetchPokeData(true);
   }, []);
+
+  useEffect(() => {
+    handleSearchInput(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   const fetchPokeData = async (isFirstFetch) => {
     try {
@@ -30,12 +37,11 @@ function App() {
     }
   };
 
-  const handleSearchInput = async (e) => {
-    setSearchTerm(e.target.value);
-    if (e.target.value) {
+  const handleSearchInput = async (searchTerm) => {
+    if (searchTerm) {
       try {
         const response = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${e.target.value}`
+          `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
         );
         const pokemonData = {
           url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
@@ -62,7 +68,7 @@ function App() {
             <input
               type="text"
               value={searchTerm}
-              onChange={handleSearchInput}
+              onChange={(e) => setSearchTerm(e.target.value)}
               className="text-xs w-[20.5rem] h-6 px-2 py-1 bg-[hsl(214,13%,47%)] rounded-lg text-gray-300 text-center"
             />
             <button
