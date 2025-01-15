3f2b7b8a590dc6de147395b701b7408b51758bd0
Jaewon Kim <jjwon.kim@gmail.com>
2025-01-16 00:17:38 +0900

    04. 포켓몬 상세 페이지 데이터 생성하기기

--- react-pokemon-app/src/pages/DetailPage/index.jsx
+++ react-pokemon-app/src/pages/DetailPage/index.jsx
@@ -1,6 +1,46 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const DetailPage = () => {
+
+  const params = useParams();
+  const pokemonId = params.id;
+  const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
+
+  useEffect(() => {
+    fetchPokemonData();
+  }, []);
+
+  async function fetchPokemonData() {
+    const url = `${baseUrl}${pokemonId}`;
+    try {
+      const {data: pokenmonData} = await axios.get(url);
+
+      if(pokenmonData) {
+        const {name, id, types, weight, height, stats, abilities} = pokenmonData;
+        const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id); 
+        console.log(nextAndPreviousPokemon)
+      }
+    } catch (error) { 
+      console.error(error);
+    }
+  }
+
+  async function getNextAndPreviousPokemon(id) {
+    const urlPokemon = `${baseUrl}?limit=1&offset=${id - 1}`;
+
+    const {data: pokenmonData} = await axios.get(urlPokemon);
+    console.log(pokenmonData)
+
+    const nextResponse = pokenmonData.next && (await axios.get(pokenmonData.next)); 
+    const previousResponse = pokenmonData.previous && (await axios.get(pokenmonData.previous));
+    return {
+      next: nextResponse?.data?.results?.[0]?.name, 
+      previous: previousResponse?.data?.results?.[0]?.name
+    };
+  }
+
   return <div>DetailPage</div>;
 };
 
