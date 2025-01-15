ecf3e536d4b9a54341ce7e9a0932c48d1eb7413a
Jaewon Kim <jjwon.kim@gmail.com>
2025-01-15 23:44:52 +0900

    04. 포켓몬 도감 앱에 React Router Dom 적용용

--- react-pokemon-app/src/App.jsx
+++ react-pokemon-app/src/App.jsx
@@ -1,113 +1,17 @@
-import { useEffect, useState } from "react";
-import axios, { all } from "axios";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import PokeCard from "./components/PokeCard";
-import { useDebounce } from "./hooks/useDebounce";
-import AutoComplete from "./components/AutoComplete";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
 
-function App() {
-  // 모든 포켓몬 데이터를 가지고 있는 State
-  const [allPokemons, setAllPokemons] = useState([]);
-  // 화면에 보여줄 포켓몬 데이터를 가지고 있는 State
-  const [displayedPokemons, setDisplayedPokemons] = useState([]);
-  // 한번에 보여주는 포캣몬 수
-  const limitNum = 20;
-  const url = `https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0`;
-
-  // const debouncedSearchTerm = useDebounce(searchTerm, 500);
-
-  useEffect(() => {
-    // API => response => state update => component rerender => state
-    fetchPokeData();
-  }, []);
-
-  // useEffect(() => {
-  //   handleSearchInput(debouncedSearchTerm);
-  // }, [debouncedSearchTerm]);
-
-  const filterDisplayedPokemonData = (
-    allPokemonsData,
-    displayedPokemonsData = []
-  ) => {
-    const limit = displayedPokemonsData.length + limitNum;
-    // 모든 포켓몬 데이터에서 limitNum만큼 더 가져오기.
-    const array = allPokemonsData.filter((_, index) => index < limit);
-    return array;
-  };
-
-  const fetchPokeData = async () => {
-    try {
-      // 1008개의 포켓몬 데이터를 가져옴
-      const response = await axios.get(url);
-      // console.log(response.data.results);
-      setAllPokemons(response.data.results);
-      // 화면에 보여줄 포켓몬 데이터를 20개 가져옴
-      setDisplayedPokemons(filterDisplayedPokemonData(response.data.results));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  // const handleSearchInput = async (searchTerm) => {
-  //   if (searchTerm) {
-  //     try {
-  //       const response = await axios.get(
-  //         `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
-  //       );
-  //       const pokemonData = {
-  //         url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
-  //         name: searchTerm,
-  //       };
-  //       setPokemon([pokemonData]);
-  //     } catch (error) {
-  //       setPokemon([]);
-  //       console.error(error);
-  //     }
-  //   } else {
-  //     fetchPokeData(true);
-  //   }
-  // };
-
+const App = () => {
   return (
-    <article className="pt-6">
-      <header className="flex flex-col gap-2 w-full px-4 z-50">
-        <AutoComplete
-          allPokemons={allPokemons}
-          setDisplayedPokemons={setDisplayedPokemons}
-        />
-      </header>
-      <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0">
-        <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl">
-          {displayedPokemons.length > 0 ? (
-            displayedPokemons.map(({ url, name }, index) => (
-              <PokeCard key={name} url={url} name={name} />
-            ))
-          ) : (
-            <h2 className="font-medium, text-lg text-slate-900 mb-1">
-              포켓몬이 없습니다.
-            </h2>
-          )}
-        </div>
-      </section>
-      <div className="text-center">
-        {allPokemons.length > displayedPokemons.length &&
-          displayedPokemons.length !== 1 && (
-            <button
-              onClick={() =>
-                setDisplayedPokemons(
-                  filterDisplayedPokemonData(allPokemons, displayedPokemons)
-                )
-              }
-              className="bg-slate-800 px-6 py-2 my-4 text-base rounded-lg font-bold text-white"
-            >
-              더보기
-            </button>
-          )}
-      </div>
-    </article>
-  );
-}
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/pokemon/:id" element={<DetailPage />} />
+    </Routes>
+    </BrowserRouter>
+  )
+};
 
 export default App;
