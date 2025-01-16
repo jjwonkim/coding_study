f871f7ed70ed222b84825a70c4d12ae65ed3b3b8
Jaewon Kim <jjwon.kim@gmail.com>
2025-01-16 20:55:41 +0900

    05. 포켓몬 데이터 가공하기

--- react-pokemon-app/src/pages/DetailPage/index.jsx
+++ react-pokemon-app/src/pages/DetailPage/index.jsx
@@ -1,8 +1,10 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
 const DetailPage = () => {
+  const [pokemon, setPokemon] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   const params = useParams();
   const pokemonId = params.id;
@@ -15,30 +17,79 @@ const DetailPage = () => {
   async function fetchPokemonData() {
     const url = `${baseUrl}${pokemonId}`;
     try {
-      const {data: pokenmonData} = await axios.get(url);
+      const { data: pokenmonData } = await axios.get(url);
 
-      if(pokenmonData) {
-        const {name, id, types, weight, height, stats, abilities} = pokenmonData;
-        const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id); 
-        console.log(nextAndPreviousPokemon)
+      if (pokenmonData) {
+        const { name, id, types, weight, height, stats, abilities } =
+          pokenmonData;
+        const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id);
+
+        const damageRelations = await Promise.all(
+          types.map(async (i) => {
+            const type = await axios.get(i.type.url);
+            return type.data.damage_relations;
+          })
+        );
+
+        const formattedPokemonData = {
+          id,
+          name,
+          weight: weight / 10,
+          height: height / 10,
+          previvous: nextAndPreviousPokemon.previous,
+          next: nextAndPreviousPokemon.next,
+          abilities: formatPokemonAbilities(abilities),
+          stats: formatPokemonStats(stats),
+          damageRelations,
+        };
+        setPokemon(formattedPokemonData);
+        setIsLoading(false);
       }
-    } catch (error) { 
+    } catch (error) {
       console.error(error);
     }
   }
 
+  const formatPokemonAbilities = (abilities) => {
+    return abilities
+      .filter((_, index) => index <= 1)
+      .map((obj) => obj.ability.name.replaceAll("-", " "));
+  };
+
+  const formatPokemonStats = ([
+    statHP,
+    statATK,
+    statDEP,
+    statSATK,
+    statSDEP,
+    statSPD,
+  ]) => [
+    { name: "Hit Points", baseStat: statHP.base_stat },
+    { name: "Attack", baseStat: statATK.base_stat },
+    { name: "Defense", baseStat: statDEP.base_stat },
+    { name: "Special Attack", baseStat: statSATK.base_stat },
+    { name: "Special Defense", baseStat: statSDEP.base_stat },
+    { name: "Speed", baseStat: statSPD.base_stat },
+  ];
+
   async function getNextAndPreviousPokemon(id) {
     const urlPokemon = `${baseUrl}?limit=1&offset=${id - 1}`;
 
-    const {data: pokenmonData} = await axios.get(urlPokemon);
-    console.log(pokenmonData)
+    const { data: pokenmonData } = await axios.get(urlPokemon);
+    console.log(pokenmonData);
 
-    const nextResponse = pokenmonData.next && (await axios.get(pokenmonData.next)); 
-    const previousResponse = pokenmonData.previous && (await axios.get(pokenmonData.previous));
+    const nextResponse =
+      pokenmonData.next && (await axios.get(pokenmonData.next));
+    const previousResponse =
+      pokenmonData.previous && (await axios.get(pokenmonData.previous));
     return {
-      next: nextResponse?.data?.results?.[0]?.name, 
-      previous: previousResponse?.data?.results?.[0]?.name
+      next: nextResponse?.data?.results?.[0]?.name,
+      previous: previousResponse?.data?.results?.[0]?.name,
     };
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
   return <div>DetailPage</div>;
